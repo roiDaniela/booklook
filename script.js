b3c567874a7c0eb70deaa8fd4443dc1770de5160
roiDaniela <roi.fogler@gmail.com>
2018-04-15 19:30:13 +0300

    search 2

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ var fetch = function () {
     $.ajax({
       method: "GET",
     //   url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
-        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + $("#isbn").val(),
+        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + $("#intitle").val(),
       success: function(data) {          
         console.log(data);
-        $(".book-title").text(data.items[0].volumeInfo.title);
-        $(".book-author").text(data.items[0].volumeInfo.authors[0]);
-        $(".book-description").text(data.items[0].volumeInfo.description);
-        $(".book-img").attr("src",data.items[0].volumeInfo.imageLinks.smallThumbnail);
-        $(".book-img").toggle();
+            for (let i = 0; i < 10; i++) {
+                if(data.items[i].volumeInfo){
+                    var divBook = document.createElement("div");
+                    var bookTitle = $(document.createElement("h1")).text(data.items[i].volumeInfo.title);;
+                    var bookAuthor = $(document.createElement("p")).text(data.items[i].volumeInfo.description)
+                    var bookDes = $(document.createElement("h2")).text(data.items[i].volumeInfo.authors[0]);
+                    var bookImg = $(document.createElement("img")).width("304").height("236").attr("src",data.items[i].volumeInfo.imageLinks.smallThumbnail);
+
+                    $(".book").after(divBook, bookTitle, bookAuthor, bookDes, bookImg);
+
+                    // $(".book-title").text(data.items[i].volumeInfo.title);
+                    // $(".book-author").text(data.items[i].volumeInfo.authors[0]);
+                    // $(".book-description").text(data.items[i].volumeInfo.description);
+                    // $(".book-img").attr("src",data.items[i].volumeInfo.imageLinks.smallThumbnail);
+                    // $(".book-img").toggle();
+                }                
+            }
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
