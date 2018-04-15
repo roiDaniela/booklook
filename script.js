c214ff631d278553f37631f1113c49ba1154c8b6
roiDaniela <roi.fogler@gmail.com>
2018-04-15 18:06:42 +0300

    fix search

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var fetch = function () {
     $.ajax({
       method: "GET",
-      url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+    //   url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + $("#isbn").val(),
       success: function(data) {          
         console.log(data);
         $(".book-title").text(data.items[0].volumeInfo.title);
